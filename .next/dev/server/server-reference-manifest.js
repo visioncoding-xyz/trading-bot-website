a4670aeff5a72699f1fe4e84bd60c65769080898
Vision Coding <raphaelalexandercardona@gmail.com>
2026-02-08 12:34:18 -0500

    Revert "[dyad] Preparing to write AI rules - wrote 1 file(s) + extra files edited outside of Dyad"
    This reverts commit 51ce5030334c1b43a01f392e1c7bdb5953188e4e.

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"QIL9xhxbviBFtDOdqswZVQqGlF95ucVaLJVhJ3EeNwo=\"\n}"
--- .next/dev/static/chunks/979b6_next_app_496632d8.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([typeof document === "object" ? document.currentScript : undefined,
-"[project]/node_modules/.pnpm/next@16.1.6_react-dom@19.2.4_react@19.2.4__react@19.2.4/node_modules/next/app.js [client] (ecmascript)", ((__turbopack_context__, module, exports) => {
-
-module.exports = __turbopack_context__.r("[project]/node_modules/.pnpm/next@16.1.6_react-dom@19.2.4_react@19.2.4__react@19.2.4/node_modules/next/dist/pages/_app.js [client] (ecmascript)");
-}),
-]);
-
-//# sourceMappingURL=979b6_next_app_496632d8.js.map
